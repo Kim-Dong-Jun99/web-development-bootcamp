e17257f3f8c5045023bd158849c804b62dd5c021
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2022-09-06 13:14:28 +0900

    dice game customize

--- Dicee Challenge/index.js
+++ Dicee Challenge/index.js
@@ -3,3 +3,10 @@ var randomNumber2 = Math.floor(Math.random() * 6);
 const dices = ["dice1.png", "dice2.png", "dice3.png", "dice4.png", "dice5.png", "dice6.png"];
 document.querySelectorAll(".dice img").item(0).setAttribute("src", "images/" + dices[randomNumber1]);
 document.querySelectorAll(".dice img").item(1).setAttribute("src", "images/" + dices[randomNumber2]);
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML="해선이 승리!!!🥳";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML="동준승 ㅎㅎ";
+} else{
+    document.querySelector("h1").innerHTML="비겼다!!"
+}
