8a9caf92584eae9daa5e36056317ea3f9d38812c
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2022-09-06 10:59:19 +0900

    dom done

--- DOM/index.js
+++ DOM/index.js
@@ -1,3 +1,6 @@
-alert("Hello");
+// alert("Hello");
 document.querySelector("h1").innerHTML="GoodBye";
-document.firstElementChild.lastElementChild.lastElementChild.lastElementChild.innerHTML = "dongjunkim"
+document.getElementsByClassName("item").item(2).innerHTML = "djkim";
+document.querySelector("#list a").style.color = "red";
+document.querySelector("button").style.backgroundColor = "yellow";
+document.querySelector("h1").classList.toggle("huge");
